386fb7519adc909791570aa1c204e0136a30c99d
Preetam Varun <preetamarun99@gmail.com>
2021-10-14 23:59:29 +0530

    completed half of the explore screen

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,9 @@
 import 'package:flutter/material.dart';
-//
-// List<Color>kColors = [Colors.blueAccent,Colors.blueAccent,Colors.lightBlueAccent,Colors.blueAccent];
+
+List<Color>kColors = [Colors.blueAccent,Colors.blueAccent,Colors.lightBlueAccent,Colors.blueAccent];
+
+const kExploreTextStyle= TextStyle(
+  color: Colors.white,
+  fontSize: 20.0,
+  fontWeight: FontWeight.w600,
+);
